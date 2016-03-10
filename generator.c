b9443253f51fb681984d7c15a93eab073ffed6f3
habbarazzer <habbarazzer@gmail.com>
2016-03-09 20:45:39 -0500

    some refactoring

--- generator.c
+++ generator.c
@@ -1,5 +1,5 @@
 /*
- * Generates files.
+ * Generates unsorted, sorted, and reverse sorted files of arbitrary length.
  * 
  * Programmer - Nick Martinez
  * CSC310 - Design and Analysis of Algorithms
@@ -8,54 +8,68 @@
 #include <stdlib.h>
 
 #define UPPER_BOUND 10000
-#define LIST_SIZE 10000
 #define NUM_FILES 30
 
-#define BUFFER_SIZE 16
+#define BUFFER_SIZE 24
 
+void generate_files(char *descriptor, int list_size);	// generates the files
 int get_random(int upper_bound);	// returns a random integer
 int comparison(const void *a, const void *b);	// comparison function needed for stdlib qsort function
 int r_comparison(const void *a, const void *b); // comparison function needed for stdlib qsort function - reversed  
 
 int main(void)
 {
-	int i = 0;
-//	for( i = 0; i < NUM_FILES; i++ )
-//	{
+	generate_files("small", 10000);
+	generate_files("medium", 100000);
+	generate_files("large", 1000000);
+
+	return EXIT_SUCCESS;
+}
+
+/*
+ * Generates NUM_FILES unsorted, sorted, and reversed sorted files, with 
+ * descriptor prepended to the filename and of size list_size.
+ * Returns nothing.
+ */
+void generate_files(char *descriptor, int list_size)
+{
+	int i;
+	for( i = 1; i <= NUM_FILES; i++ )
+	{
 		/* open new file for writing */
 		char file_name[BUFFER_SIZE+1];
-		snprintf(file_name, BUFFER_SIZE, "unsorted_%d", i);
+		snprintf(file_name, BUFFER_SIZE, "%s_unsorted_%d", descriptor, i);
 		FILE *fp_unsorted = fopen(file_name, "w+");
 		
-		snprintf(file_name, BUFFER_SIZE, "sorted_%d", i);
+		snprintf(file_name, BUFFER_SIZE, "%s_sorted_%d", descriptor, i);
 		FILE *fp_sorted = fopen(file_name, "w+");
 
-		snprintf(file_name, BUFFER_SIZE, "r_sorted_%d", i);
+		snprintf(file_name, BUFFER_SIZE, "%s_r_sorted_%d", descriptor, i);
 		FILE *fp_r_sorted = fopen(file_name, "w+");
 
 		/* generate list of integers */
-		int *data_list = (int *)malloc(sizeof(int)*LIST_SIZE);
+		int *data_list = (int *)malloc(sizeof(int)*list_size);
 		int j;
-		for( j = 0; j < LIST_SIZE; j++ )
+		for( j = 0; j < list_size; j++ )
 		{
 			data_list[j] = get_random(UPPER_BOUND);
 		}
 
-		for( j = 0; j < LIST_SIZE; j++ )
+		for( j = 0; j < list_size; j++ )
 		{
 			fprintf(fp_unsorted, "%d\n", data_list[j]);
 		}
 
 		/* sort the list of integers */
-		qsort(data_list, LIST_SIZE, sizeof(*data_list), comparison);
-		for( j = 0; j < LIST_SIZE; j++ )
+		qsort(data_list, list_size, sizeof(*data_list), comparison);
+		for( j = 0; j < list_size; j++ )
 		{
 			fprintf(fp_sorted, "%d\n", data_list[j]);
 		}
 
 		/* reverse sort the list of integers */
-		qsort(data_list, LIST_SIZE, sizeof(*data_list), r_comparison);
-		for( j = 0; j < LIST_SIZE; j++ )
+		qsort(data_list, list_size, sizeof(*data_list), r_comparison);
+		for( j = 0; j < list_size; j++ )
 		{
 			fprintf(fp_r_sorted, "%d\n", data_list[j]);
 		}
@@ -65,9 +79,7 @@ int main(void)
 		fclose(fp_r_sorted);
 
 		free(data_list);
-//	}
-
-	return EXIT_SUCCESS;
+	}
 }
 
 /**
